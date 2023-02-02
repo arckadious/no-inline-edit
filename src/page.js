04bb7ac4b0a43d886aadf301298010fe3fadd21e
arckadious <56547397+arckadious@users.noreply.github.com>
2023-02-02 12:41:43 +0100

    Update page.js

--- src/page.js
+++ src/page.js
@@ -1,4 +1,28 @@
-window.WRM = window.WRM || {};
-window.WRM._unparsedData = window.WRM._unparsedData || {};
-Object.defineProperty(window.WRM._unparsedData,
-    "com.atlassian.jira.jira-issue-nav-components:inline-edit-feature.enabled", { value: false });
+document.addEventListener('click', (event) => {
+    if (event.metaKey || event.ctrlKey) {
+        return; // allow normal click-to-edit when key is held
+    }
+    let e = event.target;
+ 
+    if (["A","IMG"].includes(e.tagName)) {
+        return; // allow normal click on normal hyperlinks
+    }
+    while (e !== document) {
+        const aa = e.attributes;
+        for (let i = 0; i < aa.length; ++i) {
+            const a = aa[i];
+            if (a.name === 'role' && a.value === 'presentation' && e.tagName !== 'SPAN') {
+                // This is an attempt to cleanly match relevant elements (comments, description) despite class obfuscation.
+                // Excluding <span> avoids inhibiting clicks on attach/link buttons.
+ 
+                console.log("CLICKED TO NOT EDIT, hold ctrl/cmd if you really want the thing to happen");
+                event.preventDefault();
+                event.stopPropagation();
+                return false;
+            }
+        }
+        e = e.parentNode;
+    }
+},
+                          { capture: true }
+                         )
